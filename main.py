72fa0956e35e1ef910ddc3ad47ef56b68a1a635c
fexxaa <s46666434@gmail.com>
2026-09-20 23:54:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def run_market_loop():
 
 async def main():
     print("[1/2] Авторизация Telegram клиента...")
-    await tg_client.start()
+    await tg_client.start(+17179244204)
 
     print("[2/2] Сброс старых подключений и запуск бота...")
     await bot.delete_webhook(drop_pending_updates=True)
